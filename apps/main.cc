2f5ae5f397d82e451872b1fac535cf68e015aa6a
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2022-11-19 17:25:19 +0100

    WIP: Re-enable parser

--- apps/main.cc
+++ apps/main.cc
@@ -3,28 +3,6 @@
 
 #include <iostream>
 
-using kaleidoscope::Token;
-
-void processAndPrint(std::string input)
-{
-    kaleidoscope::Lexer l(std::move(input));
-
-    kaleidoscope::Token next_t;
-    do {
-        next_t = l.GetNextToken();
-        if (next_t == Token::kTokenIdentifier) {
-            std::cout << kaleidoscope::TokenAsString(next_t) << ": "
-                      << l.GetIdentifier() << '\n';
-        } else if (next_t == Token::kTokenNumber) {
-            std::cout << kaleidoscope::TokenAsString(next_t) << ": "
-                      << l.GetNumber() << '\n';
-        } else if (next_t == Token::kTokenAscii) {
-            std::cout << kaleidoscope::TokenAsString(next_t) << ": "
-                      << l.GetAscii() << '\n';
-        } else
-            std::cout << kaleidoscope::TokenAsString(next_t) << '\n';
-    } while (next_t != Token::kTokenUnknown && next_t != Token::kTokenEof);
-}
 
 int main()
 {
--- include/parser/parser.h
+++ include/parser/parser.h
@@ -4,7 +4,6 @@
 #include "ast/expression.h"
 #include "ast/fn.h"
 #include "ast/fn_prototype.h"
-#include "lexer/token2.h"
 
 #include <memory>
 #include <string>
@@ -12,7 +11,6 @@
 namespace kaleidoscope
 {
 
-enum Token : signed char;
 class Lexer;
 
 class Parser
@@ -24,27 +22,27 @@ class Parser
     ~Parser();
 
     std::unique_ptr<ast::Expression> ParseNextExpression();
-    std::unique_ptr<ast::Fn> ParseNextTopLevelExpression();
+    // std::unique_ptr<ast::Fn> ParseNextTopLevelExpression();
 
    private:
-    std::unique_ptr<Lexer> m_Lexer;
-    std::unique_ptr<Token2> m_CurrentToken;
+    std::unique_ptr<Lexer> lexer_;
 
    private:
-    void AdvanceToNextToken();
-    int GetCurrBinOpTokenPrecedence() const;
+   bool IsNextToken(TokenType type);
+    // void AdvanceToNextToken();
+    // int GetCurrBinOpTokenPrecedence() const;
 
-    std::unique_ptr<ast::Expression> ParseNumberExpression();
-    std::unique_ptr<ast::Expression> ParseParenthesesExpression();
-    std::unique_ptr<ast::Expression> ParseIdentifierExpression();
-    std::unique_ptr<ast::Expression> ParsePrimaryExpression();
-    std::unique_ptr<ast::Expression> ParseExpression();
-    std::unique_ptr<ast::Expression> ParseBinaryOpRhs(
-        int expression_precedence,
-        std::unique_ptr<ast::Expression> lhs_expression);
-    std::unique_ptr<ast::FnPrototype> ParsePrototype();
-    std::unique_ptr<ast::Fn> ParseDefinition();
-    std::unique_ptr<ast::FnPrototype> ParseExtern();
+    // std::unique_ptr<ast::Expression> ParseNumberExpression();
+    // std::unique_ptr<ast::Expression> ParseParenthesesExpression();
+    // std::unique_ptr<ast::Expression> ParseIdentifierExpression();
+    // std::unique_ptr<ast::Expression> ParsePrimaryExpression();
+    // std::unique_ptr<ast::Expression> ParseExpression();
+    // std::unique_ptr<ast::Expression> ParseBinaryOpRhs(
+    //     int expression_precedence,
+    //     std::unique_ptr<ast::Expression> lhs_expression);
+    // std::unique_ptr<ast::FnPrototype> ParsePrototype();
+    // std::unique_ptr<ast::Fn> ParseDefinition();
+    // std::unique_ptr<ast::FnPrototype> ParseExtern();
 };
 }  // namespace kaleidoscope
 
