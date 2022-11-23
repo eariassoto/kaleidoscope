0b4c317e623278af22fccc3baed22b8a308401c6
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2022-11-23 19:19:38 +0100

    catch errors in main fn

--- apps/main.cc
+++ apps/main.cc
@@ -1,5 +1,6 @@
 #include <kaleidoscope/ast/number.h>
 #include <kaleidoscope/jit_interpreter.h>
+#include <kaleidoscope/lexer_error.h>
 #include <kaleidoscope/lexer.h>
 #include <kaleidoscope/parser.h>
 
@@ -11,24 +12,31 @@ int main()
     JitInterpreter interpreter;
 
     while (true) {
-        std::cout << "Eval > ";
-        std::string input;
-        getline(std::cin, input);
-        if (input == "quit") break;
+        try {
+            std::cout << "Eval > ";
+            std::string input;
+            getline(std::cin, input);
+            if (input == "quit") break;
 
-        Parser parser(std::make_unique<Lexer>(std::move(input)));
-        if (std::unique_ptr<ast::Expression> expr =
-                parser.ParseNextExpression()) {
-            if (ast::Number* number = dynamic_cast<ast::Number*>(expr.get())) {
-                interpreter.GenerateIR(*number);
-                // ugly, fix
-                std::cout << '\n';
-                continue;
+            Parser parser(std::make_unique<Lexer>(std::move(input)));
+            if (std::unique_ptr<ast::Expression> expr =
+                    parser.ParseNextExpression()) {
+                if (ast::Number* number =
+                        dynamic_cast<ast::Number*>(expr.get())) {
+                    interpreter.GenerateIR(*number);
+                    // ugly, fix
+                    std::cout << '\n';
+                    continue;
+                }
+                std::string res;
+                expr->PrintToString(res, 0, '=');
+                std::cout << res;
             }
-            std::string res;
-            expr->PrintToString(res, 0, '=');
-            std::cout << res;
+        } catch (const LexerError& lexer_err) {
+            // TODO: Improve error messages
+            std::cerr << "Lexer error: " << lexer_err.what();
         }
+        // TODO: catch other errors
     }
 
     return 0;
